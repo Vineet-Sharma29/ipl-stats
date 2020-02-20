712be7913b97d5c5a5c69bed42a344871a42631d
Vineet-Sharma29 <vineet.s17@iiits.in>
2020-02-20 23:40:57 +0530

    added src/components/graphs/RadarGraph.vue

---
+++ src/components/graphs/RadarGraph.vue
@@ -0,0 +1,55 @@
+<script>
+import { Radar, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+
+export default {
+  name: "RadarChart",
+  extends: Radar,
+  mixins: [reactiveProp],
+  props: {
+    chartData: Object,
+    legend: Boolean
+  },
+
+  data() {
+    return {
+      options: {
+        hoverBorderWidth: 20,
+        tooltips: {
+          backgroundColor: "#4F5565",
+          titleFontStyle: "normal",
+          titleFontSize: 18,
+          bodyFontFamily: "'Proxima Nova', sans-serif",
+          cornerRadius: 3,
+          bodyFontColor: "#20C4C8",
+          bodyFontSize: 14,
+          xPadding: 14,
+          yPadding: 14,
+          displayColors: false,
+          mode: "index",
+          intersect: false,
+          callbacks: {
+            title: tooltipItem => {
+              return `${tooltipItem[0].xLabel}`;
+            },
+            label: (tooltipItem, data) => {
+              let dataset = data.datasets[tooltipItem.datasetIndex];
+              let currentValue = dataset.data[tooltipItem.index];
+              return `${currentValue.toLocaleString()}`;
+            }
+          }
+        },
+        legend: {
+          display: this.legend
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
