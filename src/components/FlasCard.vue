f516d67ad8b7cd0bb0c7dc08c7b12a988f73d074
Vineet-Sharma29 <vineet.s17@iiits.in>
2020-02-20 23:39:09 +0530

    added src/components/FlasCard.vue

--- src/components/FlasCard.vue
+++ src/components/FlasCard.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="container mx-auto py-10">
-    <div
-      class="m-6 rounded-lg bg-white mx-auto max-w-sm shadow-md rounded-lg overflow-hidden"
-    >
+    <div class="m-6 bg-white border-l-4 border-blue-500 mx-auto max-w-sm shadow-md overflow-hidden">
       <div class="sm:flex sm:items-center px-6 py-4">
         <img
           class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
-          v-bind="flashCardIcon"
+          :src="flashCardIcon"
           alt="icon"
         />
         <div class="text-center sm:text-left sm:flex-grow">
           <div class="mb-4">
-            <p class="text-xl leading-tight">{{ flashCardHeading }}</p>
+            <p class="leading-tight text-sm text-gray-500">{{ flashCardHeading }}</p>
+            <p class="text-4xl font-black text-blue-500">{{flashCardVal}}</p>
           </div>
         </div>
       </div>
@@ -29,6 +28,10 @@ export default {
     flashCardHeading: {
       type: String,
       default: ""
+    },
+    flashCardVal: {
+      type: Number,
+      default: 0
     }
   }
 };
