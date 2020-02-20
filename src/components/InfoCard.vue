5d8a8b05441edf655621e5362587943f88038a64
Vineet-Sharma29 <vineet.s17@iiits.in>
2020-02-20 23:39:39 +0530

    added src/components/InfoCard.vue

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,39 +1,103 @@
 <template>
   <div class="shadow-md mx-6">
     <div class="px-6 py-4 h-full">
-      <div class="font-bold text-xl mb-2">Toss Decision and Wins</div>
-      <pie-graph
+      <div class="font-bold text-xl mb-2">{{title}}</div>
+      <pie-graph v-if="isLoaded && pie" :height="280" :chartData="datacollection" :legend="true"></pie-graph>
+      <doughnut-graph
+        v-if="isLoaded && doughnut"
         :height="280"
-        :chart-data="chartData"
-        :legend="false"
-      ></pie-graph>
+        :chartData="datacollection"
+        :legend="true"
+      />
+      <radar-graph
+        v-if="isLoaded && radar"
+        :height="280"
+        :chartData="datacollection"
+        :legend="true"
+      />
+      <polar-area-graph
+        v-if="isLoaded && polarArea"
+        :height="280"
+        :chartData="datacollection"
+        :legend="true"
+      />
     </div>
   </div>
 </template>
 <script>
 import PieGraph from "./graphs/PieGraph";
+import DoughnutGraph from "./graphs/DoughnutGraph";
+import RadarGraph from "./graphs/RadarGraph";
+import PolarAreaGraph from "./graphs/PolarAreaGraph";
 
 export default {
   name: "InfoGraph",
-  props: {},
   components: {
-    "pie-graph": PieGraph
+    "pie-graph": PieGraph,
+    "doughnut-graph": DoughnutGraph,
+    "radar-graph": RadarGraph,
+    "polar-area-graph": PolarAreaGraph
+  },
+  props: {
+    polarArea: {
+      type: Boolean,
+      default: false
+    },
+    radar: {
+      type: Boolean,
+      default: false
+    },
+    pie: {
+      type: Boolean,
+      default: false
+    },
+    doughnut: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ""
+    },
+    isLoaded: {
+      type: Boolean,
+      default: false
+    },
+    data: {
+      type: Object,
+      default: () => {}
+    },
+    bkgColor: {
+      type: Array,
+      default: () => []
+    }
   },
   data() {
     return {
-      chartData: {
-        hoverBackgroundColor: "red",
+      datacollection: null
+    };
+  },
+  created() {
+    this.setChartData(this.data);
+    console.log("chartData is ", this.data);
+  },
+  methods: {
+    setChartData(values) {
+      var labels = Object.keys(values),
+        data = Object.values(values);
+
+      this.datacollection = {
         hoverBorderWidth: 10,
-        labels: ["Green", "Red", "Blue"],
+        labels: labels,
         datasets: [
           {
-            label: "Data One",
-            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
-            data: [1, 10, 5]
+            label: this.title,
+            backgroundColor: this.bkgColor,
+            data: data
           }
         ]
-      }
-    };
+      };
+    }
   }
 };
 </script>
